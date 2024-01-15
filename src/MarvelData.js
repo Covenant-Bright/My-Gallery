741dd7b0b2893e1b4171e751e715eca9b068ac3a
Covenant-Bright <150696237+Covenant-Bright@users.noreply.github.com>
2024-01-15 15:04:21 +0100

    Add files via upload
    My-Gallery

---
+++ src/MarvelData.js
@@ -0,0 +1,95 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Chris Sanders and Dean DeBlois",
+    alias: "Train your Dragon",
+    img1: "https://e1.pxfuel.com/desktop-wallpaper/522/590/desktop-wallpaper-how-to-train-your-dragon-homecoming-2019-iphone-how-to-train-your-dragon.jpg",
+    homeworld: "",
+    abilities: [
+      "Inquisitive",
+      "Intelligent",   
+    ],
+    team: "DreamWorks",
+    description:
+      "A hapless young Viking who aspires to hunt dragons becomes the unlikely friend of a young dragon himself, and learns there may be more to the creatures than he assumed.",
+  },
+  {
+    name: "Aron Warner",
+    alias: "Shrek",
+    abilities: ["Clever", "Self-sufficient"],
+    team: "DreamWorks",
+    img1: "https://wallpapercosmos.com/w/middle-vertical-retina/3/7/4/49125-1536x2732-phone-hd-shrek-wallpaper.jpg",
+    homeworld: "",
+    description:
+      "A mean lord exiles fairytale creatures to the swamp of a grumpy ogre, who must go on a quest and rescue a princess for the lord in order to get his land back. It's the story of a terrifying green ogre by the name of Shrek, who lives in a swamp.",
+  },
+  {
+    name: "Darla K. Anderson",
+    alias: "Coco",
+    abilities: ["Family", "Music"],
+    team: "Disney",
+    img1: "https://w.forfun.com/fetch/7c/7c877649a3229bdc682599da43f08f31.jpeg?h=900&r=0.5",
+    homeworld: "",
+
+    description:
+      "The story follows a 12-year-old boy named Miguel (Gonzalez) who is accidentally transported to the Land of the Dead, where he seeks the help of his deceased musician great-great-grandfather to return him to his family among the living and to reverse his family's ban on music.",
+  },
+  {
+    name: "Peter Del Vecho",
+    alias: "Frozen",
+    abilities: ["Water and ice", "wind"],
+    team: "Disney",
+    img1: "https://e0.pxfuel.com/wallpapers/496/749/desktop-wallpaper-frozen-2-2019-poster-textless-frozen-disney-movie-frozen-disney-frozen-elsa-frozen-cartoon.jpg",
+    homeworld: "",
+
+    description:
+      "In Frozen, fearless optimist Anna teams up with rugged mountain man Kristoff and his loyal reindeer Sven in an epic journey, encountering Everest-like conditions, mystical trolls and a hilarious snowman named Olaf in a race to find Anna's sister Elsa, whose icy powers have trapped the kingdom of Arendelle in eternal ...",
+  },
+
+ 
+  {
+    name: "Hanna and Barbera",
+    alias: "Tom and Jerry",
+    abilities: ["Chase", "funny"],
+    team: "Warner",
+    img1: "https://e0.pxfuel.com/wallpapers/920/966/desktop-wallpaper-tom-and-jerry-cat-mouse.jpg",
+    homeworld: "",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Tom and Jerry is an American cartoon series about a hapless cat's never-ending pursuit of a clever mouse. Tom is the scheming cat, and Jerry is the spunky mouse.",
+  },
+
+  {
+    name: "John Walker",
+    alias: "The Incredibles",
+    abilities: ["super-hero", "Superhuman agility"],
+    team: "Pixar",
+    img1: "https://e1.pxfuel.com/desktop-wallpaper/183/65/desktop-wallpaper-the-incredibles-mr-incredible.jpg",
+    homeworld: "imaginative",
+    description:
+      " The film follows Bob and Helen Parr, a couple of superheroes, known as Mr. Incredible and Elastigirl, respectively, who hide their powers in accordance with a government mandate, and attempt to live a quiet suburban life with their three children.",
+  },
+
+  {
+    name: "Chris Meledandri, Janet Healy, John Cohen",
+    alias: "Minions",
+    abilities: ["animated comedy", "super-villian"],
+    team: "Illumination",
+    img1: "https://e1.pxfuel.com/desktop-wallpaper/814/827/desktop-wallpaper-despicable-me-3-minions-the-minions-android.jpg",
+    homeworld: "",
+    description:
+      "Despicable Me is an American media franchise created by Sergio Pablos. It centers on a former supervillain turned secret agent named Gru and his yellow-colored Minions. The franchise is produced by Illumination and distributed by its parent company Universal Pictures.",
+  },
+  {
+    name: "Osnat Shurer",
+    alias: "Moana",
+    abilities: ["Wayfinding", "Water manipulation"],
+    team: "Disney",
+    img1: "https://e1.pxfuel.com/desktop-wallpaper/1021/155/desktop-wallpaper-moana-animation-disney-movies-moana-disney.jpg",
+    homeworld: "",
+    description:
+      "Moana is a sea-loving, strong-willed wayfarer. Though she has moments of self-doubt, she has great pride in who she is, and doesn't back away from new challenges. She approaches new experiences and tasks with the utmost seriousness and will stand her ground to fight for what she values, even when all seems lost.",
+  },
+];
+
+console.log(characters);
